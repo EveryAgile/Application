67f45cf5648ab3223c5b7e80baa5866ffb7b8cc3
minsukk0 <87852284+minsukk0@users.noreply.github.com>
2022-06-08 22:06:20 +0900

    sprint backlog view
    sprint backlog view

---
+++ app/src/main/java/com/example/myapplication/activities/SprintCreateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class SprintCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_sprint_backlog)
+    }
+}
