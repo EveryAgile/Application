887036d647e4f26080a85028a498ac9fb5b3eeae
Hojin <hojin180@ajou.ac.kr>
2022-06-15 04:14:34 +0900

    add : layout

---
+++ app/src/main/java/com/example/myapplication/models/ProductLIstItem.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.models
+
+import android.widget.TextView
+
+class ProductLIstItem (val name: TextView)
---
+++ app/src/main/java/com/example/myapplication/models/ProjectListItem.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.models
+
+import androidx.appcompat.widget.AppCompatButton
+
+class ProjectListItem (val name: AppCompatButton)
