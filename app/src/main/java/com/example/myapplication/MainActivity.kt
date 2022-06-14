1575ac006a48910483324ae557f9c506ef7e24f2
Hojin <hojin180@ajou.ac.kr>
2022-06-15 04:20:05 +0900

    fix : main activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.activities.ProjectActivity
+import com.example.myapplication.activities.SignInActivity
+import com.example.myapplication.adpaters.ProjectListAdapter
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.models.ResponsResult
-import com.example.myapplication.models.SignUp
+import com.example.myapplication.models.ProjectCreate
+import com.example.myapplication.models.ProjectListItem
+import com.example.myapplication.models.ProjectResponseDto
 import com.example.myapplication.network.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,46 +25,58 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Agile 화면으로
-        binding.buttonAgile.setOnClickListener {
-            var intent = Intent(this, ProjectActivity::class.java)
-            startActivity(intent)
-        }
-//
-//        val call: Call<Users> = RetrofitClient.networkService.getUsers()
-//        call.enqueue(object : Callback<Users> {
-//            override fun onResponse(
-//                call: Call<Users>,
-//                response: Response<Users>
-//            ) {
-//                if (response.isSuccessful) {
-//                    Log.d("회원 모드 조회", "성공 : ${response.body()}")
-//                } else {
-//                    Log.d("회원 모드 조회", "실패1 : ${response.errorBody()?.string()!!}")
-//                }
-//            }
-//            override fun onFailure(call: Call<Users>, t: Throwable) {
-//                Log.d("회원 모드 조회", "실패2 : $t")
-//            }
-//        })
-        var singUp = SignUp("hojin180@gmail.com","hojin","1234")
+        val layoutManager = LinearLayoutManager(this)
+        val projectListAdapter = ProjectListAdapter()
 
-        val call: Call<ResponsResult> = RetrofitClient.networkService.signUp(singUp)
-        call.enqueue(object : Callback<ResponsResult> {
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = projectListAdapter
+        ProjectListAdapter().notifyDataSetChanged()
+
+        var accessToken = SignInActivity.prefs.getString("ACCESS_TOKEN","")
+
+        createProjectBtn()
+        getProject(accessToken, projectListAdapter)
+    }
+
+    private fun getProject(
+        accessToken: String,
+        projectListAdapter: ProjectListAdapter
+    ) {
+        val call: Call<ProjectCreate> = RetrofitClient.networkService.getProject(accessToken)
+        call.enqueue(object : Callback<ProjectCreate> {
             override fun onResponse(
-                call: Call<ResponsResult>,
-                response: Response<ResponsResult>
+                call: Call<ProjectCreate>,
+                response: Response<ProjectCreate>
             ) {
                 if (response.isSuccessful) {
-                    Log.d("회원 모드 조회", "성공 : ${response.body()}")
+                    var ProjectList: List<ProjectResponseDto>? = response.body()?.list
+                    if (ProjectList != null && ProjectList.size > 0) {
+                        for (i in 0 until ProjectList.size) {
+                            projectListAdapter.projectListData.add(ProjectList[i])
+                            projectListAdapter.notifyDataSetChanged()
+                        }
+                    } else {
+                        Log.d("Project 조회", "생성된 프로젝트가 없습니다")
+                    }
+                    response.body()?.toString()?.let { Log.d("로그인", it) }
+
                 } else {
-                    Log.d("회원 모드 조회", "실패1 : ${response.errorBody()?.string()!!}")
+                    Log.d("로그인", "실패1 : ${response.errorBody()?.string()!!}")
+                    Log.d("로그인", accessToken.toString())
                 }
             }
-            override fun onFailure(call: Call<ResponsResult>, t: Throwable) {
-                Log.d("회원 모드 조회", "실패2 : $t")
+
+            override fun onFailure(call: Call<ProjectCreate>, t: Throwable) {
+                Log.d("로그인", "실패2 : $t")
             }
         })
+    }
+
+    private fun createProjectBtn() {
+        binding.floatingActionButton.setOnClickListener {
+            var intent = Intent(this, ProjectCreateActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroy() {
